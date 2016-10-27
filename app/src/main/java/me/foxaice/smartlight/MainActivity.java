8f44b0627e406b52599c0d553e549577d56abb2d
foxaice <foxaice@yandex.com>
2016-10-27 13:35:00 +0700

    fixed splash screen bug

--- app/src/main/java/me/foxaice/smartlight/MainActivity.java
+++ app/src/main/java/me/foxaice/smartlight/MainActivity.java
@@ -7,6 +7,7 @@ public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
