0c024a5c10756c4e95a1bf5b91552d52f6cef62f
foxaice <foxaice@yandex.com>
2016-10-27 13:30:21 +0700

    added splash screen

---
+++ app/src/main/java/me/foxaice/smartlight/application/SmartLightApplication.java
@@ -0,0 +1,11 @@
+package me.foxaice.smartlight.application;
+
+import android.app.Application;
+
+public class SmartLightApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
