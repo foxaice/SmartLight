97f6ee77de7448eef697c004484adf64c7091487
foxaice <foxaice@yandex.com>
2016-10-28 17:37:40 +0700

    fixed VectorCompat drawable bug

--- app/src/main/java/me/foxaice/smartlight/activities/main_screen/MainScreenActivity.java
+++ app/src/main/java/me/foxaice/smartlight/activities/main_screen/MainScreenActivity.java
@@ -2,10 +2,14 @@ package me.foxaice.smartlight.activities.main_screen;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v7.app.AppCompatDelegate;
 
 import me.foxaice.smartlight.R;
 
 public class MainScreenActivity extends Activity {
+    static {
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
