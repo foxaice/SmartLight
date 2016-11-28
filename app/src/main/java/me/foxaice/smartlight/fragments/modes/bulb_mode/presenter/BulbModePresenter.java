64af4e0c834dce42213c53e0389c61a83a3fcc67
foxaice <foxaice@yandex.com>
2016-11-28 16:28:59 +0700

    added BrightnessArcInfo class for BulbModePresenter

--- app/src/main/java/me/foxaice/smartlight/fragments/modes/bulb_mode/presenter/BulbModePresenter.java
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/bulb_mode/presenter/BulbModePresenter.java
@@ -1,5 +1,10 @@
 package me.foxaice.smartlight.fragments.modes.bulb_mode.presenter;
 
+import android.support.annotation.IntDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import me.foxaice.smartlight.activities.main_screen.model.IBulbInfo;
 import me.foxaice.smartlight.fragments.modes.ModeBasePresenter;
 import me.foxaice.smartlight.fragments.modes.bulb_mode.view.IBulbModeView;
@@ -83,4 +88,64 @@ public class BulbModePresenter extends ModeBasePresenter<IBulbModeView> implemen
         return 0;
     }
 
+    @IntDef({Quarter.LEFT_TOP, Quarter.LEFT_BOTTOM, Quarter.RIGHT_TOP, Quarter.RIGHT_BOTTOM})
+    @Retention(RetentionPolicy.SOURCE)
+    @interface Quarter {
+        int LEFT_TOP = 1;
+        int LEFT_BOTTOM = 2;
+        int RIGHT_TOP = 3;
+        int RIGHT_BOTTOM = 4;
+    }
+
+    private static final class BrightnessArcInfo {
+        private boolean isWithinArc;
+        private boolean isProhibition;
+        @Quarter
+        private Integer prevQuarter;
+        @Quarter
+        private Integer curQuarter;
+
+        @Quarter
+        private int getQuarter(float x, float y) {
+            if (x >= 0 && y >= 0) return Quarter.RIGHT_BOTTOM;
+            else if (x >= 0 && y < 0) return Quarter.RIGHT_TOP;
+            else if (x < 0 && y >= 0) return Quarter.LEFT_BOTTOM;
+            else return Quarter.LEFT_TOP;
+        }
+
+        private void clearInfo() {
+            isWithinArc = false;
+            isProhibition = false;
+            prevQuarter = null;
+            curQuarter = null;
+        }
+
+        private void updateBrightnessArcInfo(@Events int eventAction, float deltaX, float deltaY) {
+            if (eventAction == Events.ACTION_DOWN) {
+                this.prevQuarter = this.getQuarter(deltaX, deltaY);
+                this.isProhibition = false;
+            }
+            if (eventAction == Events.ACTION_MOVE) {
+                this.curQuarter = this.getQuarter(deltaX, deltaY);
+                if (this.prevQuarter != null) {
+                    @Quarter int preQ = this.prevQuarter;
+                    @Quarter int curQ = this.curQuarter;
+                    if (this.isProhibition) {
+                        if ((preQ == Quarter.LEFT_BOTTOM && curQ != Quarter.RIGHT_BOTTOM)
+                                || (preQ == Quarter.RIGHT_BOTTOM && curQ != Quarter.LEFT_BOTTOM)) {
+                            this.isProhibition = false;
+                        }
+                    } else {
+                        if ((preQ == Quarter.LEFT_BOTTOM && curQ == Quarter.RIGHT_BOTTOM)
+                                || (preQ == Quarter.RIGHT_BOTTOM && curQ == Quarter.LEFT_BOTTOM)) {
+                            this.isProhibition = true;
+                        }
+                    }
+                    if (!this.isProhibition && curQ != preQ) {
+                        this.prevQuarter = curQ;
+                    }
+                }
+            }
+        }
+    }
 }
