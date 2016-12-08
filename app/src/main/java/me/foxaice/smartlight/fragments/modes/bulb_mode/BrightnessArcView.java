946df50a998f033fb694ad481a2dba702ebdb6aa
foxaice <foxaice@yandex.com>
2016-12-08 16:44:17 +0700

    added BrightnessArcView

---
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/bulb_mode/BrightnessArcView.java
@@ -0,0 +1,66 @@
+package me.foxaice.smartlight.fragments.modes.bulb_mode;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.AttributeSet;
+
+import me.foxaice.smartlight.R;
+
+public class BrightnessArcView extends android.support.v7.widget.AppCompatImageView {
+
+    private Bitmap mBitmapBorder;
+    private Bitmap mBitmapMask;
+    private Paint mPaintMask;
+    private Paint mPaintColor;
+    private Paint mPaintBorder;
+    private RectF mArcRect;
+    private float mAngle = 50;
+    private int mWidth;
+    private int mHeight;
+    private Rect mFrameRect;
+
+    public BrightnessArcView(Context context) {
+        this(context, null);
+    }
+
+    public BrightnessArcView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mWidth = getResources().getDimensionPixelSize(R.dimen.brightness_arc_width);
+        mHeight = getResources().getDimensionPixelSize(R.dimen.brightness_arc_height);
+        int targetRadius = getResources().getDimensionPixelSize(R.dimen.brightness_arc_target_size);
+
+        mBitmapBorder = getBitmapFromVectorDrawable(getContext(), R.drawable.bulb_mode_image_brightness_arc);
+        mBitmapMask = getBitmapFromVectorDrawable(getContext(), R.drawable.bulb_mode_image_brightness_arc_mask);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            mBitmapMask = mBitmapMask.extractAlpha();
+        }
+        mFrameRect = new Rect(0, 0, mWidth, mHeight);
+
+        mArcRect = new RectF(targetRadius, targetRadius, mWidth - targetRadius, mWidth - targetRadius);
+
+        mPaintMask = new Paint();
+        mPaintColor = new Paint();
+        mPaintBorder = new Paint();
+        mPaintMask.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));
+        mPaintColor.setStrokeWidth(targetRadius * 2);
+        mPaintColor.setStyle(Paint.Style.STROKE);
+        mPaintColor.setColor(ContextCompat.getColor(context, R.color.backgroundBrightnessArc));
+        mPaintBorder.setColor(Color.BLACK);
+    }
+
+    public BrightnessArcView(Context context, AttributeSet attrs, int defStyle) {
+        this(context, attrs);
+    }
+}
