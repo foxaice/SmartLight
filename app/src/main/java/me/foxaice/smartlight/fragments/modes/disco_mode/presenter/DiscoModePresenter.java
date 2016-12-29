db28dd580419b242c7c4fc9d23375c4697265f54
foxaice <foxaice@yandex.com>
2016-12-29 12:56:04 +0700

    added DiscoModePresenter

---
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/disco_mode/presenter/DiscoModePresenter.java
@@ -0,0 +1,25 @@
+package me.foxaice.smartlight.fragments.modes.disco_mode.presenter;
+
+import java.io.IOException;
+
+import me.foxaice.smartlight.fragments.modes.IModeBasePresenter;
+import me.foxaice.smartlight.fragments.modes.ModeBasePresenter;
+import me.foxaice.smartlight.fragments.modes.disco_mode.view.IDiscoModeView;
+
+public class DiscoModePresenter extends ModeBasePresenter<IDiscoModeView> implements IDiscoModePresenter {
+
+    @Override
+    public void onTouchSpeedUpButton(@Events int eventAction) {
+
+    }
+
+    @Override
+    public void onTouchSpeedDownButton(@Events int eventAction) {
+
+    }
+
+    @Override
+    public void onTouchNextModeButton(@Events int eventAction) {
+
+    }
+}
