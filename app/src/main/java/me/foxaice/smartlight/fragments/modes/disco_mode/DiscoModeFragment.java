6bac6105469baff9186b16ad7b4cfa838642d029
foxaice <foxaice@yandex.com>
2017-01-09 13:04:54 +0700

    minor changes

--- app/src/main/java/me/foxaice/smartlight/fragments/modes/disco_mode/DiscoModeFragment.java
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/disco_mode/DiscoModeFragment.java
@@ -1,12 +1,33 @@
 package me.foxaice.smartlight.fragments.modes.disco_mode;
 
 import me.foxaice.smartlight.fragments.modes.ModeBaseView;
+import me.foxaice.smartlight.fragments.modes.disco_mode.view.IDiscoModeView;
 
-public class DiscoModeFragment extends ModeBaseView {
+public class DiscoModeFragment  extends ModeBaseView implements IDiscoModeView {
     public static final String TAG = "DISCO_MODE_FRAGMENT";
 
     @Override
     public void onChangedControllerSettings() {
+
+    }
+
+    @Override
+    public void spinVinylImage(@Action int action) {
+
+    }
+
+    @Override
+    public void setPressedSpeedUpButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public void setPressedSpeedDownButton(boolean isPressed) {
+
+    }
+
+    @Override
+    public void setPressedNextModeButton(boolean isPressed) {
 
     }
 }
