036fc6dc6c4ae4ee8a996f59b2912d5dd6f4c919
foxaice <foxaice@yandex.com>
2017-01-27 13:38:20 +0700

    added CommonUtils

---
+++ app/src/main/java/me/foxaice/smartlight/utils/CommonUtils.java
@@ -0,0 +1,18 @@
+package me.foxaice.smartlight.utils;
+
+final class CommonUtils {
+    private CommonUtils() {
+    }
+
+    static int nextPowOf2(int value) {
+        if (isPowOf2(value)) {
+            return value;
+        } else {
+            return Integer.highestOneBit(value) << 1;
+        }
+    }
+
+    static boolean isPowOf2(int value) {
+        return (value & value - 1) == 0;
+    }
+}
