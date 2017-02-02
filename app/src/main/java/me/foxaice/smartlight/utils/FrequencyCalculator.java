58ad873e54c8b16c8c3a63be757c0efc076acd43
foxaice <foxaice@yandex.com>
2017-02-02 13:42:16 +0700

    added applyHannWindow method in FrequencyCalculator

--- app/src/main/java/me/foxaice/smartlight/utils/FrequencyCalculator.java
+++ app/src/main/java/me/foxaice/smartlight/utils/FrequencyCalculator.java
@@ -26,4 +26,13 @@ public class FrequencyCalculator {
     public double[] getFrequenciesMagnitudes() {
         return mFrequenciesMagnitudes;
     }
+
+    private double[] applyHannWindow(short[] pcm) {
+        double[] res = new double[pcm.length];
+        for (int i = 0; i < pcm.length; i++) {
+            double window = 0.5 * (1 - Math.cos((2 * Math.PI * i) / (pcm.length - 1)));
+            res[i] = pcm[i] * window;
+        }
+        return res;
+    }
 }
