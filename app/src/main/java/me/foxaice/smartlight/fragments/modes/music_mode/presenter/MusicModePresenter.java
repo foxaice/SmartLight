9736b2b0145b6b964c982018d5c3cdbaa6e5fec2
foxaice <foxaice@yandex.com>
2017-02-07 13:48:32 +0700

    added RecordThread in MusicModePresenter

--- app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/presenter/MusicModePresenter.java
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/presenter/MusicModePresenter.java
@@ -8,6 +8,7 @@ import android.os.Process;
 import me.foxaice.smartlight.fragments.modes.ModeBasePresenter;
 import me.foxaice.smartlight.fragments.modes.music_mode.model.IMusicInfo;
 import me.foxaice.smartlight.fragments.modes.music_mode.view.IMusicModeView;
+import me.foxaice.smartlight.utils.FrequencyCalculator;
 
 
 public class MusicModePresenter extends ModeBasePresenter<IMusicModeView> implements IMusicModePresenter {
@@ -28,5 +29,69 @@ public class MusicModePresenter extends ModeBasePresenter<IMusicModeView> implem
     @Override
     public void loadMusicInfoFromPreferences() {
 
+    }
+
+    private class RecordThread implements Runnable {
+        private static final int SAMPLE_RATE = 44100;
+        private static final int MAX_AMPLITUDE = 32000;
+        private static final int MIN_AMPLITUDE = 400;
+        private static final int MAX_FREQUENCY = 6000;
+        private static final int BUFFER_SIZE = 2048;
+        private boolean mIsAlive;
+        private AudioRecord mAudioRecord;
+        private short[] mAudioBuffer;
+        private double mMinVolumeThreshold;
+        private double mMaxFrequency;
+        private double mMaxVolumeThreshold;
+        private double mMultiplierColor;
+        private double mBrightnessMultiplier;
+        private int mColorsQuantity;
+        private boolean mIsReverse;
+        private int mShift;
+
+        private void startRecord() {
+            mIsAlive = true;
+            MusicModePresenter.this.executorService.submit(this);
+        }
+
+        private void stopRecord() {
+            mIsAlive = false;
+            if (mAudioRecord != null) mAudioRecord.release();
+        }
+
+        private void record() {
+            prepareAudioRecord();
+
+            if (mAudioRecord.getState() != AudioRecord.STATE_INITIALIZED) {
+                mAudioRecord.release();
+                return;
+            }
+        }
+
+
+        private void prepareAudioRecord() {
+            android.os.Process.setThreadPriority(Process.THREAD_PRIORITY_AUDIO);
+
+            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT);
+
+            if (bufferSize == AudioRecord.ERROR || bufferSize == AudioRecord.ERROR_BAD_VALUE) {
+                bufferSize = SAMPLE_RATE * 2;
+            }
+
+            mAudioBuffer = new short[bufferSize / 2];
+
+            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.DEFAULT,
+                    SAMPLE_RATE,
+                    AudioFormat.CHANNEL_IN_MONO,
+                    AudioFormat.ENCODING_PCM_16BIT,
+                    bufferSize);
+        }
+
+        @Override
+        public void run() {
+            record();
+        }
     }
 }
