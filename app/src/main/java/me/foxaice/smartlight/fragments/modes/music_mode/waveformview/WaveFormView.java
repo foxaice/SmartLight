f72da44e7cdec00cedb46d042e195bd30ea676b9
foxaice <foxaice@yandex.com>
2017-02-17 14:24:46 +0700

    added WabeFormView for MusicModeFragment

---
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/waveformview/WaveFormView.java
@@ -0,0 +1,48 @@
+package me.foxaice.smartlight.fragments.modes.music_mode.waveformview;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
+
+import me.foxaice.smartlight.R;
+import me.foxaice.smartlight.fragments.modes.music_mode.model.IMusicInfo;
+
+public class WaveFormView extends AppCompatImageView {
+    private static final int STROKE_WIDTH_DP = 2;
+    private static final int PADDING_DP = 16;
+    private Paint mStrokePaint;
+    private Paint mFillPaint;
+    private Paint mWaveFormPaint;
+    private int mStrokeWidthPx;
+    private float mLeftCanvasCoord;
+    private float mTopCanvasCoord;
+    private float mBottomCanvasCoord;
+    private float mRightCanvasCoord;
+    private float mDensity;
+    private RectF mRectF;
+    private int mMax = Short.MIN_VALUE;
+    @IMusicInfo.ViewType
+    private int mViewType;
+    private double[] mAudioBuffer;
+
+    public WaveFormView(Context context) {
+        super(context);
+        init();
+    }
+
+    public WaveFormView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public WaveFormView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+}
