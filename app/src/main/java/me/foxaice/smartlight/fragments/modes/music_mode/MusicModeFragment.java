5e449f712abb75a8a612ce89ae1f1ee20fbfbb8b
foxaice <foxaice@yandex.com>
2017-02-24 13:17:42 +0700

    minor changes

--- app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/MusicModeFragment.java
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/MusicModeFragment.java
@@ -1,12 +1,54 @@
 package me.foxaice.smartlight.fragments.modes.music_mode;
 
 import me.foxaice.smartlight.fragments.modes.ModeBaseView;
+import me.foxaice.smartlight.fragments.modes.music_mode.model.IMusicInfo;
+import me.foxaice.smartlight.fragments.modes.music_mode.view.IMusicModeView;
 
-public class MusicModeFragment extends ModeBaseView {
+public class MusicModeFragment extends ModeBaseView implements IMusicModeView {
     public static final String TAG = "MUSIC_MODE_FRAGMENT";
 
     @Override
     public void onChangedControllerSettings() {
 
+    }
+
+    @Override
+    public void drawWaveFormView(double[] data, String color, double max, @IMusicInfo.ViewType int viewType) {
+
+    }
+
+    @Override
+    public void setCurrentVolumeText(double value) {
+
+    }
+
+    @Override
+    public void setFrequencyText(int value) {
+
+    }
+
+    @Override
+    public void setMaxVolumeText(int value) {
+
+    }
+
+    @Override
+    public void setMinVolumeText(int value) {
+
+    }
+
+    @Override
+    public void setColorModeText(@IMusicInfo.ColorMode int colorMode) {
+
+    }
+
+    @Override
+    public void setWaveFormVisible(boolean visible) {
+
+    }
+
+    @Override
+    public String[] getBytesColorsFromResources() {
+        return new String[0];
     }
 }
