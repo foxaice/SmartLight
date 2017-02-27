bfce4f3128f3bcbed22fe464ca3a0fdf6ab6f210
foxaice <foxaice@yandex.com>
2017-02-27 13:17:21 +0700

    added MusicModeHandler in MusicModeFragment

--- app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/MusicModeFragment.java
+++ app/src/main/java/me/foxaice/smartlight/fragments/modes/music_mode/MusicModeFragment.java
@@ -1,17 +1,42 @@
 package me.foxaice.smartlight.fragments.modes.music_mode;
 
 import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.text.Html;
 import android.text.Spanned;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.ref.WeakReference;
 
 import me.foxaice.smartlight.R;
 import me.foxaice.smartlight.fragments.modes.ModeBaseView;
 import me.foxaice.smartlight.fragments.modes.music_mode.model.IMusicInfo;
+import me.foxaice.smartlight.fragments.modes.music_mode.presenter.IMusicModePresenter;
+import me.foxaice.smartlight.fragments.modes.music_mode.presenter.MusicModePresenter;
 import me.foxaice.smartlight.fragments.modes.music_mode.view.IMusicModeView;
+import me.foxaice.smartlight.fragments.modes.music_mode.waveformview.WaveFormView;
 
 public class MusicModeFragment extends ModeBaseView implements IMusicModeView {
     public static final String TAG = "MUSIC_MODE_FRAGMENT";
-
+    private static final String KEY_IS_PLAYING = "KEY_IS_PLAYING";
+    private static final int REQUEST_CODE = 300;
+    private final IMusicModePresenter mMusicModePresenter = new MusicModePresenter();
+    private Handler mHandler;
+    private ImageView mPlayStopButtonImage;
+    private TextView mMaxVolumeText;
+    private TextView mMinVolumeText;
+    private TextView mColorModeText;
+    private TextView mCurrentVolumeText;
+    private TextView mCurrentFrequencyText;
+    private WaveFormView mWaveFormView;
+    private ConstraintSet mCacheWaveformVisibleConstraintSet;
+    private ConstraintLayout mRootLayout;
+    private boolean mIsPlaying;
+    private boolean mIsLandscapeOrientation;
 
     @SuppressWarnings("deprecation")
     public static Spanned getColorModeSpannedFromResources(Context context, @IMusicInfo.ColorMode int name) {
@@ -90,5 +115,35 @@ public class MusicModeFragment extends ModeBaseView implements IMusicModeView {
     @Override
     public String[] getBytesColorsFromResources() {
         return new String[0];
+    }
+
+    private static class MusicModeHandler extends Handler {
+        private static final int SET_FREQUENCY = 0x0001;
+        private static final int SET_VOLUME = 0x0002;
+        private final WeakReference<MusicModeFragment> wrFragment;
+        private final String mFrequencyText;
+        private final String mVolumeText;
+
+        MusicModeHandler(MusicModeFragment fragment) {
+            this.wrFragment = new WeakReference<>(fragment);
+            mFrequencyText = fragment.getString(R.string.frequency_with_param);
+            mVolumeText = fragment.getString(R.string.volume_with_param);
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+            MusicModeFragment fragment = wrFragment.get();
+            if (fragment != null) {
+                if (msg.what == SET_FREQUENCY) {
+                    fragment.mCurrentFrequencyText.setText(
+                            String.format(mFrequencyText, msg.obj)
+                    );
+                } else if (msg.what == SET_VOLUME) {
+                    fragment.mCurrentVolumeText.setText(
+                            String.format(mVolumeText, msg.obj)
+                    );
+                }
+            }
+        }
     }
 }
